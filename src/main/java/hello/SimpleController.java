048463ab7afcf010311fdcb891e7fef3beabbc06
lihj <lihj@hzcwtech.com>
2016-05-02 11:06:08 +0800

    Initial commit

---
+++ src/main/java/hello/SimpleController.java
@@ -0,0 +1,21 @@
+package hello;
+
+/**
+ * Created by helmeter on 5/2/16.
+ */
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+
+public class SimpleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+
+}
---
+++ src/main/java/hello/UserController.java
@@ -0,0 +1,22 @@
+package hello;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by helmeter on 5/2/16.
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/{id}")
+    public User view(@PathVariable("id") Long id) {
+        User user = new User();
+        user.setId(id);
+        user.setName("zhang");
+        return user;
+    }
+
+}
